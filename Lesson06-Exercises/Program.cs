05726965e83d85bdf694b417b047d9416cabc4f4
cnobert <cnobert@nait.ca>
2024-09-19 13:55:41 -0600

    first half of class

---
+++ Lesson06-Exercises/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("-----------------------");
+Console.WriteLine("-----------------------");
+
+#region Question 1 from Sequence Structure Programming Exercises
+
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your age:");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your annual salary:");
+double salary = double.Parse(Console.ReadLine());
+// Console.WriteLine($"My name is {name}, my age is {age}, and I hope to earn ${salary:#,##0.00}");
+Console.WriteLine($"My name is {name}, my age is {age}, and I hope to earn ${salary:n}");
+#endregion
+
+Console.WriteLine("-----------------------");
+Console.WriteLine("-----------------------");
