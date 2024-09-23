b77e1601396d1b743e0aaf1506d886d0f5c21453
cnobert <cnobert@nait.ca>
2024-09-23 14:36:48 -0600

    end of class

--- Lesson07-Making-Decisions/Program.cs
+++ Lesson07-Making-Decisions/Program.cs
@@ -44,35 +44,58 @@
 //         Console.WriteLine("I have money, but I'm not hungry.");
 //     }
 // }
-//exercise: run the above code three times, each time changing the variables so that
+//exercise: run the above code four times, each time changing the variables so that
 //a different message is written to the console
 #endregion
 
 #region "if" statement with compound boolean expression
 
-bool hasMoney = false;
-bool isHungry = false;
+// bool hasMoney = true;
+// bool isHungry = false;
 
-//hasMoney == true, isHungery == true
-if(hasMoney && isHungry)
-{
-    Console.WriteLine("I'm buying myself a burger!");
-}
-//hasMoney == true, isHungry = false
-else if(hasMoney && !isHungry)
-{
-    Console.WriteLine("I have money, but I'm not hungry.");
-}
-//hasMoney == false, isHungry == true
-else if(!hasMoney && isHungry)
-{
-    Console.WriteLine("I am hungry but I have no money.");
-    Console.WriteLine("No burger for me.");
-}
-else if(!hasMoney && !isHungry)
-{
-    Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
-}
+// //hasMoney == true, isHungery == true
+// if(hasMoney && isHungry)
+// {
+//     Console.WriteLine("I'm buying myself a burger!");
+// }
+// //hasMoney == true, isHungry = false
+// else if(hasMoney && !isHungry)
+// {
+//     Console.WriteLine("I have money, but I'm not hungry.");
+// }
+// //hasMoney == false, isHungry == true
+// else if(!hasMoney && isHungry)
+// {
+//     Console.WriteLine("I am hungry but I have no money.");
+//     Console.WriteLine("No burger for me.");
+// }
+// else if(!hasMoney && !isHungry)
+// {
+//     Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
+// }
+//exercise: run the above code four times, each time changing the variables so that
+//a different message is written to the console
+#endregion
 
+#region Question 01, Decision Making Exercises 01 doc
+//https://lms.nait.ca/d2l/le/content/25527/viewContent/1173615/View
+//Write a program that will prompt for a number and display “positive” if it is 
+//greater than zero,
+//“negative” if it is less than zero, and “zero” if it is equal to zero
+Console.Write("Enter a whole number: ");
+int theAnswer = int.Parse(Console.ReadLine());
+
+if(theAnswer > 0)
+{
+    Console.WriteLine("positive");
+}
+else if (theAnswer < 0)
+{
+    Console.WriteLine("negative");
+}
+else
+{
+    Console.WriteLine("zero");
+}
 #endregion
 Console.WriteLine("<----------------------->");
