df4e91b3bd8ac2090b2ca20cde1559cf2273dbb6
cnobert <cnobert@nait.ca>
2024-09-23 13:58:57 -0600

    halfway

--- Lesson07-Making-Decisions/Program.cs
+++ Lesson07-Making-Decisions/Program.cs
@@ -18,30 +18,61 @@
 
 #region nested "if" statement
 
-bool hasMoney = false;
-bool isHungry = true;
+// bool hasMoney = false;
+// bool isHungry = false;
 
-if(isHungry == true)
-{
-    if(hasMoney == true)
-    {
-        Console.WriteLine("I'm buying myself a burger!");
-    }
-    else
-    {
-        Console.WriteLine("I am hungry but I have no money.");
-        Console.WriteLine("No burger for me.");
-    }
-}
-else
-{
-    Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
-}
+// if(isHungry == true)
+// {
+//     if(hasMoney == true)
+//     {
+//         Console.WriteLine("I'm buying myself a burger!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("I am hungry but I have no money.");
+//         Console.WriteLine("No burger for me.");
+//     }
+// }
+// // else
+// {
+//     if(hasMoney == false)
+//     {
+//         Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("I have money, but I'm not hungry.");
+//     }
+// }
 //exercise: run the above code three times, each time changing the variables so that
 //a different message is written to the console
 #endregion
 
 #region "if" statement with compound boolean expression
+
+bool hasMoney = false;
+bool isHungry = false;
+
+//hasMoney == true, isHungery == true
+if(hasMoney && isHungry)
+{
+    Console.WriteLine("I'm buying myself a burger!");
+}
+//hasMoney == true, isHungry = false
+else if(hasMoney && !isHungry)
+{
+    Console.WriteLine("I have money, but I'm not hungry.");
+}
+//hasMoney == false, isHungry == true
+else if(!hasMoney && isHungry)
+{
+    Console.WriteLine("I am hungry but I have no money.");
+    Console.WriteLine("No burger for me.");
+}
+else if(!hasMoney && !isHungry)
+{
+    Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
+}
 
 #endregion
 Console.WriteLine("<----------------------->");
--- Lesson07-Making-Decisions/obj/Debug/net8.0/Lesson07-Making-Decisions.AssemblyInfo.cs
+++ Lesson07-Making-Decisions/obj/Debug/net8.0/Lesson07-Making-Decisions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson07-Making-Decisions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60aa3d07000119bd78f5b3c63afe4a84b67761fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09dfaaaf90a05802bbf868a6a3d8ad9cace0839d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson07-Making-Decisions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson07-Making-Decisions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
