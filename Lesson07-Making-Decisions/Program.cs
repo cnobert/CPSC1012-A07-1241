136c9fcd24181bef7efc128af1ada8d058d265fe
cnobert <cnobert@nait.ca>
2024-09-25 13:51:36 -0600

    step 01

--- Lesson07-Making-Decisions/Program.cs
+++ Lesson07-Making-Decisions/Program.cs
@@ -1,78 +1,79 @@
 ﻿Console.WriteLine("<----------------------->");
 
 #region first "if" statement
-bool isBig = true;
 
-//in the parentheses we put a Boolean expression, which is
-//an expression that evaluates to true or false
-if(isBig == true)
-{
-    Console.WriteLine("It is BIG!");
-}
-else
-{
-    Console.WriteLine("It is NOT big");
-}
+// bool isBig = true;
+
+// //in the parentheses we put a Boolean expression, which is
+// //an expression that evaluates to true or false
+// if(isBig)
+// {
+//     Console.WriteLine("It is BIG!");
+// }
+// else
+// {
+//     Console.WriteLine("It is NOT big");
+// }
  
 #endregion
 
 #region nested "if" statement
 
-bool hasMoney = false;
-bool isHungry = false;
+// bool hasMoney = false;
+// bool isHungry = false;
 
-if(isHungry == true)
-{
-    if(hasMoney == true)
-    {
-        Console.WriteLine("I'm buying myself a burger!");
-    }
-    else
-    {
-        Console.WriteLine("I am hungry but I have no money.");
-        Console.WriteLine("No burger for me.");
-    }
-}
+// if(isHungry == true)
+// {
+//     if(hasMoney == true)
+//     {
+//         Console.WriteLine("I'm buying myself a burger!");
+//     }
+//     else
+//     {
+//         Console.WriteLine("I am hungry but I have no money.");
+//         Console.WriteLine("No burger for me.");
+//     }
+// }
 // else
-{
-    if(hasMoney == false)
-    {
-        Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
-    }
-    else
-    {
-        Console.WriteLine("I have money, but I'm not hungry.");
-    }
-}
+// {
+//     if(hasMoney == false)
+//     {
+//         Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("I have money, but I'm not hungry.");
+//     }
+// }
 // exercise: run the above code four times, each time changing the variables so that
 // a different message is written to the console
 #endregion
 
 #region "if" statement with compound boolean expression
 
-bool hasMoney = true;
-bool isHungry = false;
+// bool hasMoney = true;
+// bool isHungry = false;
 
-//hasMoney == true, isHungry == true
-if(hasMoney && isHungry)
-{
-    Console.WriteLine("I'm buying myself a burger!");
-}
-//hasMoney == true, isHungry = false
-else if(hasMoney && !isHungry)
-{
-    Console.WriteLine("I have money, but I'm not hungry.");
-}
-//hasMoney == false, isHungry == true
-else if(!hasMoney && isHungry)
-{
-    Console.WriteLine("I am hungry but I have no money.");
-    Console.WriteLine("No burger for me.");
-}
-else if(!hasMoney && !isHungry)
-{
-    Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
-}
+// //hasMoney == true, isHungry == true
+// if(hasMoney && isHungry)
+// {
+//     Console.WriteLine("I'm buying myself a burger!");
+// }
+// //hasMoney == true, isHungry = false
+// else if(hasMoney && !isHungry)
+// {
+//     Console.WriteLine("I have money, but I'm not hungry.");
+// }
+// //hasMoney == false, isHungry == true
+// else if(!hasMoney && isHungry)
+// {
+//     Console.WriteLine("I am hungry but I have no money.");
+//     Console.WriteLine("No burger for me.");
+// }
+// else if(!hasMoney && !isHungry)
+// {
+//     Console.WriteLine("I'm not hungry, so it's all good that I have no money.");
+// }
 //exercise: run the above code four times, each time changing the variables so that
 //a different message is written to the console
 #endregion
@@ -103,6 +104,7 @@ else // if(theAnswer == 0)
 #region notes on boolean operators
 /*
     == equality operator (if both sides equal, returns true)
+    != does not equal
     && "and" operator (if both sides true, returns true)
     || "or" operator (if one side true, returns true))
     ! "not" operator (returns the opposite of the value it operates on)
---
+++ Lesson08-Decision-Exercises/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("<---------------------->");
+Console.WriteLine("<---------------------->");
+Console.WriteLine();
+Console.WriteLine();
+
+#region notes on boolean operators
+/*
+    == equality operator (if both sides equal, returns true)
+    != not equal operator (if both sides are not equal, returns true)
+    && "and" operator (if both sides true, returns true)
+    || "or" operator (if one side true, returns true))
+    ! "not" operator (returns the opposite of the value it operates on)
+    < "strictly less than" (if left side strictly less, returns true))
+    > "strictly greater than" (if left side stricty great than, returns true))
+    <= "less than or equal to" (if left side less than or equal to, returns true)
+    >= "great than or equal to" (if left side greater than or equal to, returns true))
+*/
+#endregion
+
+#region "!" operator and "!=" operator
+
+Console.Write("Enter a number: ");
+int theNumber = int.Parse(Console.ReadLine());
+
+if(theNumber != 100)
+{
+    Console.WriteLine("The number is not 100.");
+}
+else
+{
+    Console.WriteLine("The number IS 100!!!!!!");
+}
+
+bool isBig = true;
+
+if(!isBig)
+{
+    Console.WriteLine("NOT BIG");
+}
+else
+{
+    Console.WriteLine("BIG");
+}
+
+#endregion
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("<---------------------->");
+Console.WriteLine("<---------------------->");
+Console.WriteLine();
+Console.WriteLine();
