91fa69a0dc045e0cda4e70aa6c91cf570c98f1a1
cnobert <cnobert@nait.ca>
2024-11-06 13:24:55 -0700

    early in class

--- Lesson11-Decision-Review-and-Loops-Intro/obj/Debug/net8.0/Lesson11-Decision-Review-and-Loops-Intro.AssemblyInfo.cs
+++ Lesson11-Decision-Review-and-Loops-Intro/obj/Debug/net8.0/Lesson11-Decision-Review-and-Loops-Intro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson11-Decision-Review-and-Loops-Intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4baa5b52059a55eca1a6fb289ca28005fab1b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea870ba8294731ab7bc4202d1633b6afe329224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson11-Decision-Review-and-Loops-Intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson11-Decision-Review-and-Loops-Intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lesson21-Chart/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #region declare arrays and load test data
+        
+        const int ArraySize = 30;
+
+        //the following few lines contain test data - you can use this technique in the assignment if you like
+        //in this example, logicalSize equals countOfEntries in the assignment
+        int logicalSize = 20;
+
+        //20 names
+        string[] names = { "Akira", "Beatriz", "Chen", "Dmitri", "Elena", "Fatima", "Giovanni", "Hiroshi", "Isabel", "Javier", "Khaled", "Leila", "Ming", "Nina", "Omar", "Priya", "Ravi", "Sofia", "Tomoko", "Youssef" };
+
+        int[] favouriteNumbers = new int[ArraySize];
+
+        //loads up the favouriteNumbers array with random values between 1 and 500
+        Random random = new Random();
+        for (int c = 0; c < logicalSize; c++)
+        {
+            favouriteNumbers[c] = random.Next(1, 500); 
+        }
+        #endregion
+        
+        for (int c = 0; c < logicalSize; c++)
+        {
+            Console.WriteLine($"{names[c]} has favourite number {favouriteNumbers[c]}.");
+        }
+    }
+}
